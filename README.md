af1c5c90b37eb41e94d49749261e783fcb7429db
Amir <amirbagh@buffalo.edu>
2020-10-21 10:51:11 -0600

    update pwa

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # SmartForceps PWA
 The SmartForceps program as a Progressive Web App
 
-![SmartForceps PWA Screenshot](./screenshot.png "SmartForceps PWA Screenshot")
+![SmartForceps PWA Screenshot](./screenshot.jpg "SmartForceps PWA Screenshot")
 
 ## What It Is
 
 A PWA that provides Login gateway to SmartForceps data app utilizing an app manifest, service workers and caching. This app is written in plain-vanilla HTML, CSS and Javascript (ES6). It has no dependencies.
+
+## To test this out
+1. Open your terminal (NOTE: git bash preferred/ Terminal on VS Code)
+2. Navigate to the project folder where you have cloned this repo.
+3. Type the following command in the terminal
+     ```
+     yarn add serve
+     yarn serve
+     ```
+4. Visit the localhost to see your PWA in action. 
 
 ## Browser Compatibility
 
--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-var cacheName = 'smartforceps-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
-];
+window.addEventListener('load', ()=>{
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+  if('serviceWorker' in navigator){
+    try {
+      navigator.serviceWorker.register('serviceWorker.js');
+      console.log("Service Worker Registered");
+    } catch (error) {
+      console.log("Service Worker Registration Failed");
+    }
+  }
+
+
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+
+
